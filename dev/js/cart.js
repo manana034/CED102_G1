8446b312512353bb2eda58bb136c303fd1d9d80a
Alison-Chen <jane2627n@gmail.com>
2021-03-26 17:49:21 +0800

    0326

---
+++ dev/js/cart.js
@@ -0,0 +1,12 @@
+//cart.html
+const ncBtn = document.querySelector('.nc .nc-btn');
+const cart = document.querySelector('.cart');
+const close = document.querySelector('.close');
+
+ncBtn.addEventListener('click',function(){
+    cart.classList.toggle('opencart');
+})
+
+close.addEventListener('click',function(){
+    cart.classList.remove('opencart');
+})
