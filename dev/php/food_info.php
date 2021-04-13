1ffdb937e4978a8a97f043e209fdd7c9082f5450
coldfarmer <76575201+coldfarmer@users.noreply.github.com>
2021-04-13 14:02:47 +0800

    增加食物日記修改介面

---
+++ dev/php/food_info.php
@@ -0,0 +1,18 @@
+<?php 
+try {
+  require_once("connect.php");
+    // $sql = "select * from dietrecord where mNo=:mNo AND date(dtTime)=:dtTime";
+    $sql = "select * from 
+            dietrecord a join dtritem b on a.dtRNo=b.dtRNo
+            where mNo=:mNo AND date(dtTime)=:dtTime";
+    $member = $pdo->prepare($sql);
+    $member->bindValue(":mNo", $_GET["mNo"]);
+    $member->bindValue(":dtTime", $_GET["dtTime"]);
+    $member->execute();
+  	$prodRows =  $member->fetchAll(PDO::FETCH_ASSOC);
+    $result = $prodRows;
+	  echo json_encode($result);
+} catch (PDOException $e) {
+	
+}
+?>
