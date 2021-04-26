51e964203a67a901bf2c9c336cba5ab1d46b53b9
manana034 <manana034@gmail.com>
2021-04-26 11:46:15 +0800

    0426修後台

--- dev/php/getFoodData.php
+++ dev/php/getFoodData.php
@@ -1,19 +1,19 @@
-<?php 
-try {
-	require_once("../../connect_ced102g1.php");
-	$sql = "SELECT * FROM food
-		WHERE !(fdType = 9 ) OR mNo=:mNo
-	";
+<?php
+	try {
+		require_once("../../connect_ced102g1.php");
+		$sql = "SELECT * FROM food
+			WHERE !(fdType = 9 ) OR mNo=:mNo
+		";
 
-	$foods = $pdo->prepare($sql);
+		$foods = $pdo->prepare($sql);
 
-    $foods->bindValue(":mNo", $_GET["mNo"]);
+		$foods->bindValue(":mNo", $_GET["mNo"]);
 
-	$foods->execute();
+		$foods->execute();
 
-	$foodRows = $foods->fetchAll(PDO::FETCH_ASSOC);
-	echo json_encode($foodRows);
-} catch (PDOException $e) {
-	echo $e->getMessage();
-}
+		$foodRows = $foods->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($foodRows);
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+	}
 ?>
